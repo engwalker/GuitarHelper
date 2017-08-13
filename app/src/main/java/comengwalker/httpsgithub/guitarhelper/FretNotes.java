c61413353534de85595799a5f3c315ad7b542ac7
jeremy <engwalker@gmail.com>
2017-08-13 07:58:24 -0500

    initial commit

---
+++ app/src/main/java/comengwalker/httpsgithub/guitarhelper/FretNotes.java
@@ -0,0 +1,19 @@
+package comengwalker.httpsgithub.guitarhelper;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jeremy on 8/13/17.
+ */
+
+class FretNotes extends AppCompatActivity
+{
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
